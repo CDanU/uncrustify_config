375d812a8537ead0186d3a06ef152ffe2015bf13
Daniel Chumak <chumak.daniel.dev@gmail.com>
2017-12-15 15:31:50 +0100

    switch to webpack3

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,77 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin("css/app.css");
+const extractCSS = new ExtractTextPlugin("css/app.css");
 
 module.exports = {
+    entry: ['./js/buildCss.js', './js/main.ts'],
+    output:
+    {
+        filename: './js/app.js',
+    },
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.png$/,
+                use: [{
+                    loader: 'url-loader',
+                    options:
+                    {
+                        limit: 10000,
+                    },
+                }],
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract({
+                    use:
+                    [{
+                        loader: "css-loader"
+                    }],
+                    fallback: "style-loader"
+                }),
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: extractSass.extract({
+                    use:
+                    [
+                        {
+                            loader: "css-loader"
+                        },
+                        {
+                            loader: "sass-loader"
+                        }
+                    ],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            },
+        ],
+    },
+    plugins: [extractSass],
+    resolve:
+    {
+        extensions: ['.ts', '.js'],
+        modules:
+        [
+            'node_modules',
+            'bower_components',
+            'js',
+        ],
+    },
     target: "web",
-    node:{
+    node:
+    {
         process: false,
         Buffer: false,
         __filename: false,
         __dirname: false
     },
     devtool: 'source-map',
-    entry: ['./js/buildCss.js', './js/main.ts'],
-    module: {
-        loaders: [
-            {
-                test: /\.png$/,
-                loader: 'url-loader?limit=10000'
-            },
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader'
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-            },
-            {
-                test: /\.(scss|sass)$/,
-                loader: ExtractTextPlugin.extract( 'style', 'css?sourceMap!sass?sourceMap' )
-            }
-        ]
-    },
-    output: {
-        filename: './js/app.js'
-    },
-    plugins: [ new ExtractTextPlugin('./css/app.css') ],
-    resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'],
-        modulesDirectories: ['node_modules', 'bower_components', 'js']
-    },
-    sassLoader: {
-        includePaths: [ './css' ]
-    }
 };
